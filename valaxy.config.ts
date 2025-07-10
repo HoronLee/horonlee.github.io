ce3e2ed7438e0557525708aa3d32c8e2bc3e49af
HoronLee <horonlee@foxmail.com>
2025-07-10 19:15:18 +0800

    更换waline后端

--- valaxy.config.ts
+++ valaxy.config.ts
@@ -66,7 +66,7 @@ export default defineValaxyConfig<UserThemeConfig>({
   addons: [
     addonWaline({
       // Waline 配置项，参考 https://waline.js.org/reference/client/props.html
-      serverURL: 'https://waline.horon.top',
+      serverURL: 'https://waline.horonlee.com',
     }),
     addonComponents(),
   ],
