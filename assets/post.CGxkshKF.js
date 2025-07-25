7288789fdd74c2781103d9c6de06ea1c263ab21b
HoronLee <HoronLee@users.noreply.github.com>
2025-07-25 04:37:41 +0000

    deploy: 3224ed7cdafcddeaf5ab20849e5ce10317917190

---
+++ assets/post.CGxkshKF.js
@@ -0,0 +1,1 @@
+import"./chunks/@vueuse/motion.D4kSV8m4.js";import{o as t}from"./app.CXExZcD0.js";import{A as e}from"./framework.cg7GsfgJ.js";function m(o){if(!o)return{color:"",icon:"",styles:void 0};const r=t();o in r.value.types||(o="link");const s=r.value.types[o].color,i=r.value.types[o].icon,n=e(()=>o?{"--card-c-primary":s}:void 0);return{color:s,icon:i,styles:n}}export{m as u};
